8dcc62bf224a2e930d958243405587610c3ef082
tmk616window <tmk616window@gmail.com>
2024-10-12 19:14:37 +0900

    ogen 導入

--- server/main.go
+++ server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"server/ogen"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	h, err := ogen.NewServer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start the server
+	if err := http.ListenAndServe(":8080", h); err != nil {
+		log.Fatal(err)
+	}
 }
