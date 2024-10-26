d1bc0810ba74fe65d4e74cf8487053c07e5a11c5
tmk616window <tmk616window@gmail.com>
2024-10-26 19:00:18 +0900

    seed 修正

--- server/seed/seed.sql
+++ server/seed/seed.sql
@@ -1,28 +1,34 @@
-INSERT INTO priorities (id, name)
+INSERT INTO priorities (id, name, created_at, updated_at)
 VALUES 
-    (1, 'low'),
-    (2, 'middle'),
-    (3, 'high')
+    (1, 'low', NOW(), NOW()),
+    (2, 'middle', NOW(), NOW()),
+    (3, 'high', NOW(), NOW())
 ON CONFLICT (id) 
 DO UPDATE SET 
-    name = EXCLUDED.name;
+    name = EXCLUDED.name,
+    created_at = EXCLUDED.created_at,
+    updated_at = EXCLUDED.updated_at;
 
-INSERT INTO todos (id, title, description, name, priority_id)
+INSERT INTO todos (id, title, description, name, priority_id, created_at, updated_at)
 VALUES
-    (1, 'Sample Title 1', 'Sample description 1', 'Name 1', 1),
-    (2, 'Sample Title 2', 'Sample description 2', 'Name 2', 2)
+    (1, 'Sample Title 1', 'Sample description 1', 'Name 1', 1, NOW(), NOW()),
+    (2, 'Sample Title 2', 'Sample description 2', 'Name 2', 2, NOW(), NOW())
 ON CONFLICT (id)
 DO UPDATE SET
     title = EXCLUDED.title,
     description = EXCLUDED.description,
     name = EXCLUDED.name,
-    priority_id = EXCLUDED.priority_id;
+    priority_id = EXCLUDED.priority_id,
+    created_at = EXCLUDED.created_at,
+    updated_at = EXCLUDED.updated_at;
 
-INSERT INTO status (id, value)
+INSERT INTO status (id, value, created_at, updated_at)
 VALUES 
-    (1, '未着手'),
-    (2, '着手'),
-    (3, '完了')
+    (1, '未着手', NOW(), NOW()),
+    (2, '着手', NOW(), NOW()),
+    (3, '完了', NOW(), NOW())
 ON CONFLICT (id)
 DO UPDATE SET
-    value = EXCLUDED.value;
+    value = EXCLUDED.value,
+    created_at = EXCLUDED.created_at,
+    updated_at = EXCLUDED.updated_at;
