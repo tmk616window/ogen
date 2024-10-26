9c7e7dfba333a724a8a71e1b5318821a4f5c075c
tmk616window <tmk616window@gmail.com>
2024-10-26 18:50:23 +0900

    time default 削除

--- server/migrations/20241026085817_migration_name.sql
+++ server/migrations/20241026085817_migration_name.sql
@@ -1,6 +1,6 @@
 -- Modify "priorities" table
-ALTER TABLE "priorities" ADD COLUMN "created_at" timestamptz NOT NULL DEFAULT NOW(), ADD COLUMN "updated_at" timestamptz NOT NULL DEFAULT NOW();
+ALTER TABLE "priorities" ADD COLUMN "created_at" timestamptz NOT NULL, ADD COLUMN "updated_at" timestamptz NOT NULL;
 -- Modify "status" table
-ALTER TABLE "status" ADD COLUMN "created_at" timestamptz NOT NULL DEFAULT NOW(), ADD COLUMN "updated_at" timestamptz NOT NULL DEFAULT NOW();
+ALTER TABLE "status" ADD COLUMN "created_at" timestamptz NOT NULL, ADD COLUMN "updated_at" timestamptz NOT NULL;
 -- Modify "todos" table
-ALTER TABLE "todos" ADD COLUMN "created_at" timestamptz NOT NULL DEFAULT NOW(), ADD COLUMN "updated_at" timestamptz NOT NULL DEFAULT NOW();
+ALTER TABLE "todos" ADD COLUMN "created_at" timestamptz NOT NULL, ADD COLUMN "updated_at" timestamptz NOT NULL;
